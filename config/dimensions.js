40a1f1d3201e7761da61eb447505ea8e8dd3d361
QQSuperDogGaming <75622557+QQSuperDogGaming@users.noreply.github.com>
2025-07-20 01:16:41 +0800

    Create dimensions.js

---
+++ config/dimensions.js
@@ -0,0 +1,5 @@
+const dimensions = {
+  default: { tileset: "tilesets/sky.png", style: "pixel" },
+  animeWorld: { tileset: "tilesets/anime.png", style: "2D anime" },
+  lowPoly: { tileset: "tilesets/lowpoly.png", style: "3D low poly" }
+};
